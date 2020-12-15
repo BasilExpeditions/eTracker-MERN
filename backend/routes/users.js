da9a88fbecf416d6f227c68e621de185042cd977
Bastien Durand <basilexpeditions@gmail.com>
2020-12-15 18:25:57 +1100

    Cleaned up code and cleaned up indentation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,21 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
-    User.find() // Mongoose method get a list of users from mongoDB Atlas db - find() returns a promise - .then - .catch
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err))
+router.route("/").get((req, res) => {
+  User.find() // Mongoose method get a list of users from mongoDB Atlas db - find() returns a promise - .then - .catch
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
 
-    const newUser = new User({username})
+  const newUser = new User({ username });
 
-    newUser.save() // Mongoose method - returns a promise - .then - .catch
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  newUser
+    .save() // Mongoose method - returns a promise - .then - .catch
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
