edbc3cba9ce770c8e46fa1342af3519aa752e08e
Bastien Durand <basilexpeditions@gmail.com>
2020-12-15 16:01:40 +1100

    Deleted prop transfer testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import '../App.css';
-import PropComponent from './PropComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 function App() {
 
-  const orange = "Orange is here"
-
-
   return (
     <Router>
-      <div className="App">
-        <h1>App component</h1>
-          <PropComponent name={"I am inside App Component"}/>
-      </div>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
     </Router>
   );
 }
