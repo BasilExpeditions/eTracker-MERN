0a9ad0e115c364324a9d870d43bcdc39b31a5edb
Bastien Durand <basilexpeditions@gmail.com>
2020-12-16 11:48:38 +1100

    Connected frontend --> backend with axios to read and write (Get and post) users and exercises

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class ExerciseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then((response) => {
+        this.setState({ exercises: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <div>Exercise List Component</div>;
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
   }
 }
 
